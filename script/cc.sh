b658b671d998fecddfd8cd6a73dd8a000016fcec
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-25 14:09:51 +0800

    cc脚本复制过来

---
+++ script/cc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+emcc -O3 c/index.c c/tiamalgamation.c -o src/tulip_wasm.js \
+  -s SINGLE_FILE=1 \
+  -s MODULARIZE=1 \
+  -s EXPORT_NAME='tulip_wasm' \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -s EXPORTED_FUNCTIONS='[
+    "_free_task",
+    "_free_current",
+    "_reset",
+    "_init",
+    "_new_task",
+    "_inputs_number",
+    "_inputs_map",
+    "_options_number",
+    "_outputs_number",
+    "_get_inputs_number",
+    "_inputs_offset",
+    "_outputs_offset",
+    "_link_task",
+    "_run_task",
+    "_run",
+    "_sum",
+    "_mdb"
+  ]'
