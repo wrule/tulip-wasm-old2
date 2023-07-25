aca14e43d745cb9e0d1ffd3209359d5aca4466bd
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-25 17:58:56 +0800

    TulipX接口

--- src/meta.ts
+++ src/meta.ts
@@ -12,3 +12,20 @@ interface Indicator {
   option_names: string[];
   output_names: string[];
 }
+
+export
+interface TulipX {
+  _push(indic_index: number, size: number, start_task: number): number;
+  _pop(): void;
+  _inputs(task_index: number, data_index: number): number;
+  _options(task_index: number): number;
+  _outputs(task_index: number, data_index: number): number;
+  _inputs_map(
+    task_index: number, input_index: number, enabled: number,
+    target_index: number, is_inputs: number, data_index: number,
+  ): void;
+  _inputs_offset(task_index: number): number;
+  _outputs_offset(task_index: number): number;
+  _run(task_index: number): void;
+  _run_batch(start_index: number, end_index: number): void;
+}
