9d25d6238f44de162a6e62c8d58db947d614043f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-25 22:35:54 +0800

    测速

--- src/dev.ts
+++ src/dev.ts
@@ -7,12 +7,13 @@ async function dev() {
   
   const list = Array(10000000).fill(0).map(() => Math.random());
   // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const old_time = Date.now();
   const task = tulipx._push(72, list.length, 0);
   tulipx._set_array(tulipx._inputs(task, 0), list);
   tulipx._set_array(tulipx._options(task), [2]);
   tulipx._run(task);
   const result = tulipx._get_array(tulipx._outputs(task, 0), list.length);
-  console.log(result);
+  console.log(Date.now() - old_time, result.length);
 }
 
 dev();
