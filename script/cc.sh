6de4c9cb66d337fea4009b4e36eaf0b76ea61377
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-25 14:17:31 +0800

    10个导出的函数

--- script/cc.sh
+++ script/cc.sh
@@ -5,21 +5,14 @@ emcc -O3 tulipx/tulipx.c tulipx/tiamalgamation.c -o src/tulipx.js \
   -s EXPORT_NAME='tulipx' \
   -s ALLOW_MEMORY_GROWTH=1 \
   -s EXPORTED_FUNCTIONS='[
-    "_free_task",
-    "_free_current",
-    "_reset",
-    "_init",
-    "_new_task",
-    "_inputs_number",
+    "_push",
+    "_pop",
+    "_inputs",
+    "_options",
+    "_outputs",
     "_inputs_map",
-    "_options_number",
-    "_outputs_number",
-    "_get_inputs_number",
-    "_inputs_offset",
     "_outputs_offset",
-    "_link_task",
-    "_run_task",
     "_run",
-    "_sum",
-    "_mdb"
+    "_run_batch",
+    "_erase_batch"
   ]'
