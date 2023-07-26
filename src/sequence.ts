c47c25c00ddf8e7c8bcd40994171db4b1f396051
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-26 18:37:19 +0800

    继续完善sequence

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,4 @@
+import indicators from './indicators.json';
 
 export
 interface InputMap {
@@ -13,10 +14,29 @@ export
 interface Task {
   id: number;
   indic_index: number;
+  inputs_data: (ArrayLike<number> | InputMap)[],
+  options_data: ArrayLike<number>,
   inputs: { [name: string]: InputMap },
   outputs: { [name: string]: InputMap },
 }
 
 function is_arraylike(input: Input) {
   return (input as ArrayLike<number>).length != null;
+}
+
+export
+class Sequence {
+  private tasks: Task[] = [];
+
+  public get Size() {
+    return 0;
+  }
+
+  public get Outputs() {
+    return indicators[this.tasks[this.tasks.length - 1].indic_index].outputs;
+  }
+
+  public Push(task: Task) {
+    this.tasks.push(task);
+  }
 }
