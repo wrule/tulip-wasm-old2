a7b079ad7f87ae9b249f10e124f5d2a426c600c1
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-26 12:20:04 +0800

    开始处理align

--- src/dev.ts
+++ src/dev.ts
@@ -5,20 +5,20 @@ async function dev() {
   await init();
   const tulipx: TulipX = Global.tulipx_wasm;
   
-  const list = Array(10000000).fill(0).map(() => Math.random());
-  // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // const list = Array(10000000).fill(0).map(() => Math.random());
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let old_time = Date.now();
-  let result = run(72, [list], [2]);
-  console.log(Date.now() - old_time, result.length);
-  old_time = Date.now();
-  result = run(72, [list], [2]);
-  console.log(Date.now() - old_time, result.length);
-  old_time = Date.now();
-  result = run(72, [list], [2]);
-  console.log(Date.now() - old_time, result.length);
-  old_time = Date.now();
-  result = run(72, [list], [2]);
-  console.log(Date.now() - old_time, result.length);
+  let result = run(72, [list], [4]);
+  console.log(Date.now() - old_time, result);
+  // old_time = Date.now();
+  // result = run(72, [list], [2]);
+  // console.log(Date.now() - old_time, result.length);
+  // old_time = Date.now();
+  // result = run(72, [list], [2]);
+  // console.log(Date.now() - old_time, result.length);
+  // old_time = Date.now();
+  // result = run(72, [list], [2]);
+  // console.log(Date.now() - old_time, result.length);
 }
 
 dev();
