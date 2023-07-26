f132e7a91cc80d164af2ebcf929979a46e36f3a6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-26 13:26:01 +0800

    开始改造_align

--- src/meta.ts
+++ src/meta.ts
@@ -87,7 +87,7 @@ export
 function _align(outputs: Float64Array[], length: number) {
   outputs.forEach((output) => {
     const diff = length - output.length;
-    if (diff > 0) output.unshift(...Array(diff).fill(NaN));
-    if (diff < 0) output.splice(0, -diff);
+    // if (diff > 0) output.unshift(...Array(diff).fill(NaN));
+    if (diff < 0) output.subarray(-diff, output.length);
   });
 }
