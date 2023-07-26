359571b2ff1869dff56a47b1dc88e48bbeacd65f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-26 12:11:18 +0800

    缓存命中优化

--- src/dev.ts
+++ src/dev.ts
@@ -7,8 +7,17 @@ async function dev() {
   
   const list = Array(10000000).fill(0).map(() => Math.random());
   // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const old_time = Date.now();
-  const result = run(72, [list], [2]);
+  let old_time = Date.now();
+  let result = run(72, [list], [2]);
+  console.log(Date.now() - old_time, result.length);
+  old_time = Date.now();
+  result = run(72, [list], [2]);
+  console.log(Date.now() - old_time, result.length);
+  old_time = Date.now();
+  result = run(72, [list], [2]);
+  console.log(Date.now() - old_time, result.length);
+  old_time = Date.now();
+  result = run(72, [list], [2]);
   console.log(Date.now() - old_time, result.length);
 }
 
