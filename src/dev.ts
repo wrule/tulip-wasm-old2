1d85e9000e9f2b511d60fcbee59314b67b74551e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-26 13:15:53 +0800

    输入还是使用number[]效率更高

--- src/dev.ts
+++ src/dev.ts
@@ -5,7 +5,7 @@ async function dev() {
   await init();
   const tulipx: TulipX = Global.tulipx_wasm;
   
-  const list = new Float64Array(Array(10000000).fill(0).map(() => Math.random()));
+  const list = Array(10000000).fill(0).map(() => Math.random());
   // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let old_time = Date.now();
   let result = run(72, [list], [4]);
