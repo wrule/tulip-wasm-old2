9e77f898e771288219e51253bd919ec5ff913464
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-26 14:45:18 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -1,11 +1,18 @@
-import { TulipX, init, run } from './meta';
+import { TulipX, init, run, submit } from './meta';
 import { Global } from './utils';
 
 async function dev() {
   await init();
   const tulipx: TulipX = Global.tulipx_wasm;
   
-  const list = Array(10000000).fill(0).map(() => Math.random());
+  // const list = Array(10000000).fill(0).map(() => Math.random());
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const a = submit(72, list.length, [list], [2]);
+  const b = submit(72, list.length, [a.outputs.sma], [2]);
+  tulipx._run_batch(0, 1);
+  console.log(tulipx._get_array(tulipx._outputs(b.task, 0), list.length));
+
+  return;
   // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let old_time = Date.now();
   let result = run(72, [list], [2]);
