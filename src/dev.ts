7e64f1e7c5edef3670fe7db351ecdf73cb3c7e3b
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-27 16:44:47 +0800

    整理dev

--- src/dev.ts
+++ src/dev.ts
@@ -4,21 +4,23 @@ import { submit, sequence } from './sequence';
 
 async function dev() {
   await init();
-  const tulipx: TulipX = Global.tulipx_wasm;
+
+  console.log('你好，世界');
+
+  // const tulipx: TulipX = Global.tulipx_wasm;
   
-  const list = Array(100000).fill(0).map(() => Math.random());
-  // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let old_time = Date.now();
-  const seq = sequence(() => {
-    const a = submit(72, [list], [2]);
-    const b = submit(72, [a.outputs.sma], [3]);
-    return submit(72, [b.outputs.sma], [3]);
-  });
-  const result = seq.Run();
-  console.log(Date.now() - old_time, result.length);
+  // const list = Array(100000).fill(0).map(() => Math.random());
+  // // const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // let old_time = Date.now();
+  // const seq = sequence(() => {
+  //   const a = submit(72, [list], [2]);
+  //   const b = submit(72, [a.outputs.sma], [3]);
+  //   return submit(72, [b.outputs.sma], [3]);
+  // });
+  // const result = seq.Run();
+  // console.log(Date.now() - old_time, result.length);
 
-  return;
-
+  // return;
 }
 
 dev();
